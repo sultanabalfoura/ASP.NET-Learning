ddde130209b82057a687c3ba9d530f8832f1f1c0
Sultana Balfoura <95704452+sultanabalfoura@users.noreply.github.com>
2024-01-16 16:29:55 +0400

    Delete CodeAcademySystem solution directory

--- CodeAcademySystem solution/CodeAcademySystem/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeAcademySystem.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
